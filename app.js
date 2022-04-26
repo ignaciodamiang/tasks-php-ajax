bbaf29c643626bf11fb0476f256f31727acea5b4
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-25 22:06:38 -0300

    show search results.

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 $(document).ready(function () {
-  $(function () {
-    $('#search').keyup(function () {
+  $('#task-result').hide();
+
+  $('#search').keyup(function () {
+    if ($('#search').val()) {
       let search = $('#search').val();
-      $.ajax(
-        {
-          url: 'task-search.php',
-          type: 'POST',
-          data: {
-            search,
-          },
-          success: function (response) {
-            console.log(response);
-          },
+      $.ajax({
+        url: 'task-search.php',
+        type: 'POST',
+        data: {
+          search,
         },
-        'json'
-      );
-    });
+        success: function (response) {
+          let tasks = JSON.parse(response);
+          let template = '';
+          tasks.forEach((task) => {
+            template += `<li>
+                ${task.name}
+              </li>`;
+            $('#container').html(template);
+            $('#task-result').show();
+          });
+        },
+      });
+    }
   });
 });
